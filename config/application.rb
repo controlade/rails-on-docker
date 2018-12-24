c5ac0a635f5b924b30b34536e1b2d4bc4231e61d
controlade <adelgado@controlade.com>
2018-12-23 19:34:00 -0500

    edit application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,22 @@ Bundler.require(*Rails.groups)
 
 module Drkiq
   class Application < Rails::Application
+    # We want to set up a custom logger which logs to STDOUT.
+    # Docker expects your application to log to STDOUT/STDERR and to be ran
+    # in the foreground.
+    config.log_level = :debug
+    config.log_tags  = [:subdomain, :uuid]
+    config.logger    = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+
+    # Since we're using Redis for Sidekiq, we might as well use Redis to back
+    # our cache store. This keeps our application stateless as well.
+    config.cache_store = :redis_store, ENV['CACHE_URL'],
+                         { namespace: 'drkiq::cache' }
+
+    # If you've never dealt with background workers before, this is the Rails
+    # way to use them through Active Job. We just need to tell it to use Sidekiq.
+    config.active_job.queue_adapter = :sidekiq
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
